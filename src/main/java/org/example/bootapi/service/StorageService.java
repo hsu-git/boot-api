790facd3c646e75ac6dc5a140405339be7d1b8fd
hsu-git <hsu080206@gmail.com>
2025-04-16 12:33:19 +0900

    ..

--- src/main/java/org/example/bootapi/service/StorageService.java
+++ src/main/java/org/example/bootapi/service/StorageService.java
@@ -1,11 +1,10 @@
-// 설명서
 package org.example.bootapi.service;
 
 import org.springframework.web.multipart.MultipartFile;
 
 public interface StorageService {
-    // 2가지 전략
-    // 1. 파일명 - 파일명.. 나머지는 서버가 관리..
-    // 2. URL - 괜히 서버를 거치는 것도 ..
-    String upload(MultipartFile file);
+    // 2가지
+    // 1. 파일명 - 파일명만...
+    // 2. URL - 괜히 서버를 거치는 것도...
+    String upload(MultipartFile file) throws Exception; // 어떠한 형태가 나올지 모르기 때문에...
 }
